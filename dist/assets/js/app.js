7b0a3ef46e9fac1223b75f013297af52a1ca0f2d
Rostislav <moregan123456789@gmail.com>
2022-11-08 22:03:16 +0300

    fix

---
+++ dist/assets/js/app.js
@@ -0,0 +1,47 @@
+function testWebP(callback){
+    let webP = new Image();
+    webP.onload = webP.onerror = function (){
+        callback(webP.height == 2)
+    };
+    webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+}
+testWebP(function(support){
+    if(support == true){
+        document.querySelector('html').classList.add('webp');
+    } else {
+        document.querySelector('html').classList.add('no-webp');
+    }
+});
+//=============================================================================================================================================
+
+const buttons = document.querySelectorAll('.footer__inner-title');
+
+for (let item of buttons) {
+    item.addEventListener('click', (event) => {
+        const $this = event.target;
+        const elem = document.getElementById($this.getAttribute('data-tab'));
+        elem.classList.toggle('footer__list_active');
+    })
+}
+
+AOS.init({
+    // Global settings:
+    disable: 'mobile', // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+    
+  
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 150, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 500, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: true, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
